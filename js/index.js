a19449639823ad7c79f678e1b36f402b2e3170c6
Atom Macumber <amacumber@eagleproductivity.com>
2016-08-10 09:05:40 -0400

    Center camera on user
    Signed-off-by: Atom Macumber <amacumber@eagleproductivity.com>

--- js/index.js
+++ js/index.js
@@ -3,3 +3,18 @@ var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v9'
 });
+
+// Center on user.
+navigator.geolocation.getCurrentPosition(positionUpdate, positionError);
+
+function positionUpdate (position) {
+	console.log(position.coords);
+	map.flyTo({
+		center: [position.coords.longitude, position.coords.latitude], 
+		zoom: 15
+	});
+}
+
+function positionError (error) {
+	alert(error.message);
+}
