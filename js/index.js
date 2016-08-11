68b2f5cf663d3d3d4225a375cb9656042ebca118
suicidepills <atom.mac@gmail.com>
2016-08-10 23:13:09 -0400

    Debug UI, user marker, lotsa bunch stuff.
    Signed-off-by: suicidepills <atom.mac@gmail.com>

--- js/index.js
+++ js/index.js
@@ -4,17 +4,38 @@ var map = new mapboxgl.Map({
   style: 'mapbox://styles/mapbox/streets-v9'
 });
 
+// Create a marker to represent the user.
+var marker = new mapboxgl.Marker()
+  .setLngLat([0, 0])
+  .addTo(map);
+
 // Center on user.
-navigator.geolocation.getCurrentPosition(positionUpdate, positionError);
+var navOptions = {
+  enableHighAccuracy: true,
+  timeout: 10000,
+  maximumAge: 2000
+};
+var watchId = navigator.geolocation.watchPosition(positionUpdate, positionError, navOptions);
 
 function positionUpdate (position) {
-	console.log(position.coords);
-	map.flyTo({
-		center: [position.coords.longitude, position.coords.latitude], 
-		zoom: 15
-	});
+
+  // Update debug UI
+  updateDebug(position);
+  document.getElementById('debug-freshness').innerHTML = ':)';
+
+  marker.setLngLat([position.coords.longitude, position.coords.latitude])
+  map.flyTo({
+    center: [position.coords.longitude, position.coords.latitude], 
+    zoom: 15
+  });
 }
 
 function positionError (error) {
-	alert(error.message);
+  document.getElementById('debug-freshness').innerHTML = ':(';
+}
+
+function updateDebug (position) {
+  document.getElementById('debug-latitude').innerHTML = position.coords.latitude;
+  document.getElementById('debug-longitude').innerHTML = position.coords.longitude;
+  document.getElementById('debug-accuracy').innerHTML = position.coords.accuracy;
 }
